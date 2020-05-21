b1ec9fb352f5f255b82ab2439167b39a3fbdf6d8
McGarvelcuddy <mcgarvelcuddy@gmail.com>
2020-05-21 15:21:08 -0400

    Fixed issues in issue queue

--- src/MonopolyCard.js
+++ src/MonopolyCard.js
@@ -14,19 +14,16 @@ export class MonopolyCard extends SimpleColors {
       .card {
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
         width: 300px;
-        border-layout: square;
         padding: 10px;
       }
       .card:hover {
         box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
       }
       .outline {
-        border-layout: square;
         border: 1px solid black;
         padding: 15px;
       }
       .titleblock {
-        border-layout: square;
         border: 1px solid black;
         padding: 10px;
         background-color: var(--simple-colors-default-theme-accent-8);
@@ -37,6 +34,12 @@ export class MonopolyCard extends SimpleColors {
       .itemdescription, .title {
         text-align: center;
         color: var(--simple-colors-default-theme-accent-2);
+      }
+      .itemdescription {
+        font-size: 15px;
+      }
+      .title {
+        font-size: 25px;
       }
       .slotbody {
         flex: 1;
@@ -50,14 +53,17 @@ export class MonopolyCard extends SimpleColors {
 
     static get properties() {
         return {
-            itemdescription: { type: String },
+            itemDescription: { type: String },
+            itemDescription: { attribute: "item-description" },
             title: { type: String },
+            font: { type: String },
         };
     }
 
     constructor() {
         super();
         this.title = 'Title';
+        this.font = 'arial'
     }
 
     render() {
@@ -65,8 +71,8 @@ export class MonopolyCard extends SimpleColors {
       <div class="card">
         <div class="outline">
           <div class="titleblock">
-            ${this.itemdescription ? html`<h4 class="itemdescription">${this.itemdescription}</h4>` : html``}
-            <h1 class="title">${this.title}</h1>
+            ${this.itemDescription ? html`<div class="itemdescription" style="font-family: ${this.font}">${this.itemDescription}</div><br>` : html``}
+            <div class="title" style="font-family: ${this.font}">${this.title}</div>
           </div>
           <div class="listcontainer">
             <div class="slotbody">
